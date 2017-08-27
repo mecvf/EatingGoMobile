897b4ffa7ebf479a57df79090aa7088cb3201a2f
¨Eugenia¨ <mauge58@gmail.com>
2017-08-27 00:25:26 -0300

    ConLogout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AboutPage } from '../pages/about/about';
-
+import { Nav, Platform, MenuController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage = AboutPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -20,4 +22,13 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  logout() {
+    this.storage.ready().then(() => {
+      this.storage.remove('user');
+      
+      this.nav.setRoot(AboutPage);
+    });
+  }
+
 }
